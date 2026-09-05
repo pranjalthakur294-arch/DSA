023f49dbc4c8d3eb4d4fdd27233dc6fcc38692f5
pranjalthakur294-arch <pranjalthakur294@gmail.com>
2026-09-05 16:10:54 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.86%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> dp(n+2);
+
+        dp[0]=0;
+        dp[1]=0;
+
+        for (int i=n-1;i>=0;i--){
+            dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
+        }
+        
+        return dp[0];
+    }
+};
