9eb69c31932b262d35678342b4785f30953d99fe
pranjalthakur294-arch <pranjalthakur294@gmail.com>
2026-09-26 04:13:21 +0530

    Time: 3 ms (41.18%), Space: 12.8 MB (37.61%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,23 +2,32 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> m;
-        stack<int>s;
+        stack<int>st;
+        vector<int> num3;
 
-        for(int i=nums2.size()-1;i>=0;i--){
-            while(s.size()>0 && s.top()<=nums2[i]){
-                s.pop();
-            }
-            if(s.empty()){
-                m[nums2[i]]=-1;
-            }else{
-                m[nums2[i]]=s.top();
-            }
-            s.push(nums2[i]);
+        for(int i=0;i<nums2.size();i++){
+            int current=nums2[i];
+
+        if (st.empty()) st.push(current);
+        else{
+       
+            while(!st.empty() && current > st.top()){
+                m[st.top()]=current;
+                st.pop();
+                    }
+            st.push(current);
         }
-        vector<int> ans;
-        for(int i=0;i<nums1.size();i++){
-            ans.push_back(m[nums1[i]]);
-        }
-        return ans;
+    }
+   while(!st.empty()){
+    m[st.top()]=-1;
+    st.pop();
+   }
+
+   for(int j=0;j<nums1.size();j++){
+        int trial=nums1[j];
+        num3.push_back(m[trial]);
+   }
+
+   return num3;
     }
 };
